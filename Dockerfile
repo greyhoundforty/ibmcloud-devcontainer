749fd78d496918a050c0a2188ba0eac1ee05ce6c
greyhoundforty <ryantiffany@fastmail.com>
2023-05-15 14:02:36 -0500

    🐛 fix(Dockerfile): remove locale configuration from Dockerfile and add it to install-base.sh ✨ feat(install-base.sh): add build-essential package The locale configuration has been removed from the Dockerfile and added to the install-base.sh script. This is because the Dockerfile should only contain instructions for building the image, while the install-base.sh script is responsible for installing dependencies. The build-essential package has been added to the script to ensure that all necessary tools are available for building the application.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM ubuntu:22.04 as builder
 
-RUN apt-get update && apt-get install -y locales && rm -rf /var/lib/apt/lists/* \
-    	&& localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
-
-ENV LANG en_US.UTF-8
 COPY install-base.sh install-base.sh
 # COPY install-ibmcloud.sh install-ibmcloud.sh
 
--- install-base.sh
+++ install-base.sh
@@ -1,10 +1,14 @@
 set -e
 export SHELLOPTS
 
+apt-get update
+
+apt-get install -y locales && rm -rf /var/lib/apt/lists/* && localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+
 echo ">> Installing base dependencies..."
 apt-get install -y \
   apt-transport-https \
-  ca-certificates \
+  build-essential \
   curl \
   git \
   grep \
