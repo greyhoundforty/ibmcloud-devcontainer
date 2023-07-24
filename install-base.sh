b704d6cecab82becfb16ab1a0b1437bd6334154a
greyhoundforty <ryantiffany@fastmail.com>
2023-07-24 11:09:22 -0500

    chore(install-base.sh): update base installation script The base installation script has been updated to use the Ubuntu Focal base image instead of the previous version. This ensures that the latest version of Ubuntu is used as the base for the installation. Additionally, the script now installs additional packages and dependencies required for various tools and utilities, such as Ansible, SoftLayer, and Terraform. The script also includes the installation of the IBM Cloud CLI and its plugins. Finally, the script sets the default command to "/bin/bash" to provide an interactive shell when the container is run.

--- install-base.sh
+++ install-base.sh
@@ -1,75 +1,41 @@
-set -e
-export SHELLOPTS
+FROM ubuntu:focal
+ENV DEBIAN_FRONTEND noninteractive
+ENV TF_VERSION 1.5.3
+ 
+ENV pip_packages "ansible softlayer"
+ 
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+        apt-transport-https \
+        gcc \
+        ca-certificates \
+        curl \
+        git \
+        gnupg \
+        jq \
+        libssl-dev \
+        lsb-release \
+        openssh-client \
+        python3-dev \
+        python3-gssapi \
+        python3-pip \
+        python3-netaddr \
+        python3-jmespath \
+        python3-setuptools \
+        python3-wheel \
+        unzip \
+    && rm -rf /var/lib/apt/lists/* \
+    && rm -Rf /usr/share/doc && rm -Rf /usr/share/man \
+    && apt-get clean
+ 
+RUN pip install --upgrade pip \
+    && pip install $pip_packages
 
-apt-get update
+RUN curl -O https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip \
+    && unzip terraform_${TF_VERSION}_linux_amd64.zip -d /usr/bin \
+    && rm -f terraform_${TF_VERSION}_linux_amd64.zip \
+    && chmod +x /usr/bin/terraform
 
-apt-get install -y locales && rm -rf /var/lib/apt/lists/* && localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+RUN curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
 
-echo ">> Installing base dependencies..."
-apt-get install -y \
-  apt-transport-https \
-  build-essential \
-  curl \
-  git \
-  grep \
-  jq \
-
-# Terraform
-echo ">> Installing TFSwitch"
-curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
-
-# Kubernetes
-echo ">> Installing kubectl"
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl --retry 10 --retry-delay 5 -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-mv kubectl /usr/local/bin/kubectl
-chmod +x /usr/local/bin/kubectl
-
-# Helm
-echo ">> Installing helm 3"
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-
-# OpenShift CLI
-echo ">> Installing OpenShift"
-curl -LO https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable/openshift-client-linux.tar.gz
-tar zxvf openshift-client-linux.tar.gz
-mv oc /usr/local/bin/oc
-rm -rf README.md kubectl openshift-client-linux.tar.gz
-
-# Enable GIT for all directories to avoid prompt like `fatal: unsafe repository ('/app' is owned by someone else)`
-git config --global --add safe.directory '*'
-
-# IBM Cloud CLI
-echo ">> Installing ibmcloud cli"
-curl -fsSL https://clis.cloud.ibm.com/install/linux > /tmp/bxinstall.sh
-sh /tmp/bxinstall.sh
-rm /tmp/bxinstall.sh
-
-# IBM Cloud CLI plugins
-echo ">> Installing ibmcloud plugins"
-ibmcloud_plugins=( \
-  code-engine \
-  cloud-databases \
-  cloud-dns-services \
-  cloud-functions \
-  cloud-internet-services \
-  cloud-object-storage \
-  container-registry \
-  container-service \
-  vpc-infrastructure \
-  schematics \
-  tg \
-)
-for plugin in "${ibmcloud_plugins[@]}"
-do
-  ibmcloud plugin install $plugin -f -r "IBM Cloud"
-done
-
-ibmcloud config --check-version=false
-
-rm -rf /root/.bluemix/tmp /tmp/*
-
-# Test snap install
-snap install --edge starship
-
-# Clean up
-rm -rf /var/cache/apk/*
+CMD    ["/bin/bash"]
