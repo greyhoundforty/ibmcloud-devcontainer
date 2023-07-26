615c62de906e1084d172467bcc156d5b6a1dd115
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2023-07-26 16:40:49 -0500

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,6 @@ set -e
 set -u
 set -o pipefail
 
-git clone --branch=main --depth=1 https://github.com/greyhoundforty/tycho-devbox.git
-mv tycho-devbox/.devcontainer .devcontainer
-rm -rf tycho-devbox
+git clone --branch=main --depth=1 https://github.com/greyhoundforty/ibmcloud-devcontainer.git
+mv ibmcloud-devcontainer/.devcontainer .devcontainer
+rm -rf ibmcloud-devcontainer
